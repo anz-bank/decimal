38796546ecc035644509f6ea4511764947cc5ede
Marcelo Cantos <marcelo.cantos@gmail.com>
2018-11-04 19:53:48 +1100

    Test (uint128T).divBy10

--- uint128_test.go
+++ uint128_test.go
@@ -25,3 +25,34 @@ func TestUint128Sqrt(t *testing.T) {
 	require.EqualValues(t, 1<<32, uint128T{0, 1}.sqrt())
 	require.EqualValues(t, 2<<32, uint128T{0, 4}.sqrt())
 }
+
+func TestUint128DivBy10(t *testing.T) {
+	require.Equal(t, uint128T{0, 0}, uint128T{0, 0}.divBy10())
+	require.Equal(t, uint128T{0, 0}, uint128T{1, 0}.divBy10())
+	require.Equal(t, uint128T{0, 0}, uint128T{9, 0}.divBy10())
+	require.Equal(t, uint128T{1, 0}, uint128T{10, 0}.divBy10())
+	require.Equal(t, uint128T{1, 0}, uint128T{19, 0}.divBy10())
+	require.Equal(t, uint128T{2, 0}, uint128T{20, 0}.divBy10())
+	require.Equal(t, uint128T{9, 0}, uint128T{99, 0}.divBy10())
+	require.Equal(t, uint128T{10, 0}, uint128T{100, 0}.divBy10())
+	require.Equal(t, uint128T{9999, 0}, uint128T{99999, 0}.divBy10())
+	require.Equal(t, uint128T{10000, 0}, uint128T{100000, 0}.divBy10())
+	require.Equal(t, uint128T{(1 << 64) / 10, 0}, uint128T{0, 1}.divBy10())
+	require.Equal(t, uint128T{(2 << 64) / 10, 0}, uint128T{0, 2}.divBy10())
+	require.Equal(t,
+		uint128T{((123 << 64) / 10) % (1 << 64), ((123 << 64) / 10) >> 64},
+		uint128T{0, 123}.divBy10(),
+	)
+	require.Equal(t,
+		uint128T{((123456789 << 64) / 10) % (1 << 64), ((123456789 << 64) / 10) >> 64},
+		uint128T{0, 123456789}.divBy10(),
+	)
+	require.Equal(t,
+		uint128T{((123 << 56 << 64) / 10) % (1 << 64), ((123 << 56 << 64) / 10) >> 64},
+		uint128T{0, 123 << 56}.divBy10(),
+	)
+	require.Equal(t,
+		uint128T{((1<<128 - 1) / 10) % (1 << 64), ((1<<128 - 1) / 10) >> 64},
+		uint128T{1<<64 - 1, 1<<64 - 1}.divBy10(),
+	)
+}
