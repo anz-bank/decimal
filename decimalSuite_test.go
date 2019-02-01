6d6ae9018ff3d07eb3f7589afce4755e4b86fcf2
Joshua <joshuacarpeggiani@gmail.com>
2019-02-01 14:46:43 +1100

    Add structs for testcases, Delete error function

--- decimalSuite_test.go
+++ decimalSuite_test.go
@@ -4,27 +4,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
-	"strings"
 	"testing"
 )
 
-// master tester for the suite
+type decValContainer struct {
+	val1           Decimal64
+	val1Err        error
+	val2           Decimal64
+	val2Err        error
+	expectedResult Decimal64
+	expecValErr    error
+}
+type testCaseStrings struct {
+	name           string
+	op             string
+	val1           string
+	val2           string
+	expectedResult string
+}
+
+// master tester for the suite, currently uses print statements but will implement assert statements as test cases pass
 func TestFromSuite(t *testing.T) {
 	// require := require.New(t)
-	testVals, testNames := getInput("dectest/ddAdd.decTest")
-
-	var testName string
+	testVals := getInput("dectest/ddAdd.decTest")
 	var TestResult string
 	var TestPass bool
-	var testVal1, testVal2, testAns Decimal64
-	var parseResult string
+	for i := range testVals {
+		Dec64Vals := convertToDec64(testVals[i])
+		TestPass, TestResult = doTests(Dec64Vals, testVals[i]) // do more here
 
-	for i := range testNames {
-		testName = testNames[i]
-		testVal1, testVal2, testAns, parseResult = convertToDec64(testVals[testName], testName)
-		TestPass, TestResult = doTests(testVal1, testVal2, testAns, testVals[testName]["TestFunc"])
 		if TestPass == false {
-			fmt.Printf("%s: \n %s\n %s\n", testName, TestResult, parseResult)
+			fmt.Printf("\n%s: \n %s\n", testVals[i].name, TestResult)
+		}
+		if Dec64Vals.val1Err != nil || Dec64Vals.val2Err != nil || Dec64Vals.expecValErr != nil {
+			fmt.Printf("\nError parsing in test: %s: \n val 1:%s: \n Val2: %s\n val 3: %s \n", testVals[i].name, Dec64Vals.val1Err, Dec64Vals.val2Err, Dec64Vals.expecValErr)
+
 		}
 	}
 
@@ -42,94 +56,68 @@ func TestFromSuite(t *testing.T) {
 // }
 
 //getInput gets the test file and extracts test using regex, then returns a map object and a list of test names
-func getInput(file string) (map[string]map[string]string, []string) {
+func getInput(file string) (data []testCaseStrings) {
 	dat, _ := ioutil.ReadFile("dectest/ddAdd.decTest")
 	dataString := string(dat)
-	//  "(\n|\r)") // start with newline
+	//  "(\n)") // start with newline
 	//  "(?P<TestName>dd[\w]*)") // TestName must start with dd (double decimal)
 	//  "(?P<TestFunc>[\S]*)") // testfunc made of anything that isn't a whitespace
 	//  "(\s*\'?)") // after can be any number of spaces and quotations if they exist
-	// "(?P<TestVals_1>(\+|-)*[^\r\n\t\f\v\' ]*)") // first test val is anything that isnt a whitespace or a quoteation mark
-	//  "(?P<TestVals_1>(\+|-)*[^\r\n\t\f\v\' ]*)") // first test val is anything that isnt a whitespace or a quoteation mark
+	// "(?P<TestVals_1>(\+|-)*[^\t\f\v\' ]*)") // first test val is anything that isnt a whitespace or a quoteation mark
+	//  "(?P<TestVals_1>(\+|-)*[^\t\f\v\' ]*)") // first test val is anything that isnt a whitespace or a quoteation mark
 	// ('?\s*'?) match any quotation marks and any spaces
-	// (?P<TestVals_2>(\+|-[^->])?[^\r\n\t\f\v\' ]*) testvals2 same as 1 but no '->'
+	// (?P<TestVals_2>(\+|-[^->])?[^\t\f\v\' ]*) testvals2 same as 1 but no '->'
 	// ('?\s*->\s*'?) matches the indicator to answer
-	// (?P<answer>(\+|-)*[^\r\n\t\f\v\' ]*) matches the answer that's anything that is plus minus but not quotations
-	//
+	// (?P<answer>(\+|-)*[^\t\f\v\' ]*) matches the answer that's anything that is plus minus but not quotations
 	// split into groups: testName, TestFunct, TestVals (x2) and TestAns
-	regex := `(\n|\r)(?P<TestName>dd[\w]*)(\s*)(?P<TestFunc>[\S]*)(\s*\'?)(?P<TestVals_1>(\+|-)*[^\r\n\t\f\v\' ]*)('?\s*'?)(?P<TestVals_2>(\+|-[^->])?[^\r\n\t\f\v\' ]*)('?\s*->\s*'?)(?P<answer>(\+|-)*[^\r\n\t\f\v\' ]*)`
+	regex := `(\n)(?P<TestName>dd[\w]*)(\s*)(?P<TestFunc>[\S]*)(\s*\'?)(?P<TestVals_1>(\+|-)*[^\t\f\v\' ]*)('?\s*'?)(?P<TestVals_2>(\+|-[^->])?[^\t\f\v\' ]*)('?\s*->\s*'?)(?P<answer>(\+|-)*[^\r\n\t\f\v\' ]*)`
 	r := regexp.MustCompile(regex)
 	ans := r.FindAllStringSubmatch(dataString, -1)
-	testList := []string{}
-	var data = map[string]map[string]string{}
-
-	for i := 0; i < len(ans); i++ {
-		data[ans[i][2]] = make(map[string]string)
-		data[ans[i][2]]["TestFunc"] = ans[i][4]
-		data[ans[i][2]]["TestVal1"] = ans[i][6]
-		data[ans[i][2]]["TestVal2"] = ans[i][9]
-		data[ans[i][2]]["TestAns"] = ans[i][12]
-		testList = append(testList, ans[i][2])
+	var datum testCaseStrings
+	for i := range ans {
+		datum.op = ans[i][4]
+		datum.val1 = ans[i][6]
+		datum.val2 = ans[i][9]
+		datum.expectedResult = ans[i][12]
+		datum.name = ans[i][2]
+		data = append(data, datum)
 
 	}
-	return data, testList
+	return data
 
 }
 
 //convertToDec64 converts the map object strings to decimal64s
-func convertToDec64(testVals map[string]string, testName string) (testVal1, testVal2, testAns Decimal64, parseErr string) {
-	var err1, err2, err3 error
-	// var TestResult []string
-
-	testVal1, err1 = ParseDecimal64(testVals["TestVal1"])
-	testAns, err3 = ParseDecimal64(testVals["TestAns"])
-
-	testVal2, err2 = ParseDecimal64(testVals["TestVal2"])
-	parseErr = checkErrors(err1, err2, err3, testVals)
-
+func convertToDec64(testvals testCaseStrings) (dec64Vals decValContainer) {
+	dec64Vals.val1, dec64Vals.val1Err = ParseDecimal64(testvals.val1)
+	dec64Vals.expectedResult, dec64Vals.expecValErr = ParseDecimal64(testvals.expectedResult)
+	dec64Vals.val2, dec64Vals.val2Err = ParseDecimal64(testvals.val2)
 	return
 }
 
-//checkErrors checks to see if there are any string parsing errors
-func checkErrors(err1, err2, err3 error, testVals map[string]string) (errReturn string) {
-	var errReturnSlice []string
-	if err2 != nil {
-		errReturnSlice = append(errReturnSlice, fmt.Sprintf("error parsing input of value: %s \n", testVals["TestVal2"]))
-	}
-	if err1 != nil {
-		errReturnSlice = append(errReturnSlice, fmt.Sprintf("error parsing input of value: %s \n", testVals["TestVal1"]))
-	}
-
-	if err3 != nil {
-		errReturnSlice = append(errReturnSlice, fmt.Sprintf("error parsing input of value: %s \n", testVals["TestAns"]))
-	}
-	return strings.Join(errReturnSlice, "")
-}
-
+// TODO: get doTests to run more functions
 //doTests completes the tests and returns a boolean and string on if the test passes
-func doTests(testVal1, testVal2, testAns Decimal64, testfunc string) (testStatus bool, testString string) {
-
-	fmt.Println()
-	switch testfunc {
+func doTests(testVals decValContainer, testValStrings testCaseStrings) (testStatus bool, testString string) {
+	switch testValStrings.op {
 	case "add":
-		testString = fmt.Sprintf("%v + %v != %v \n(expected %v)", testVal1, testVal2, testVal1.Add(testVal2), testAns)
-		testStatus = testAns == testVal1.Add(testVal2)
+		testString = fmt.Sprintf("%v + %v != %v \n(expected %v)", testValStrings.val1, testValStrings.val2, testVals.val1.Add(testVals.val2), testVals.expectedResult)
+		testStatus = testVals.expectedResult == testVals.val1.Add(testVals.val2)
 		return
 	case "abs":
 
-		testStatus = testAns == testVal1.Abs()
+		// testStatus = testAns == testVal1.Abs()
 		return
 	case "divide":
 
-		testStatus = testAns == testVal1.Quo(testVal2)
+		// testStatus = testAns == testVal1.Quo(testVal2)
 		return
 	case "minus":
 
-		testStatus = testAns == testVal1.Sub(testVal2)
+		// testStatus = testAns == testVal1.Sub(testVal2)
 		return
 	case "multiply":
-		testString = fmt.Sprintf("%v * %v != %v (expected %v)", testVal1, testVal2, testVal1.Mul(testVal2), testAns)
-		testStatus = testAns == testVal1.Mul(testVal2)
+		// testString = fmt.Sprintf("%v * %v != %v (expected %v)", testVal1, testVal2, testVal1.Mul(testVal2), testAns)
+		// testStatus = testAns == testVal1.Mul(testVal2)
 		return
 	default:
 		testStatus = false
