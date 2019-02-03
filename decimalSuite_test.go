d0b033691068762e7fac7aae5961e28646b9d15a
Joshua <joshuacarpeggiani@gmail.com>
2019-02-03 22:19:03 +1100

    change variable names

--- decimalSuite_test.go
+++ decimalSuite_test.go
@@ -15,8 +15,8 @@ type decValContainer struct {
 	val1, val2, expected parseResult
 }
 type testCaseStrings struct {
-	name           string
-	op             string
+	testName       string
+	testFunc       string
 	val1           string
 	val2           string
 	expectedResult string
@@ -34,10 +34,10 @@ func TestFromSuite(t *testing.T) {
 		TestFailed, TestResult = doTest(Dec64Vals, testVals[i]) // do more here
 
 		if TestFailed {
-			fmt.Printf("\n%s: \n %s\n", testVals[i].name, TestResult)
+			fmt.Printf("\n%s: \n %s\n", testVals[i].testName, TestResult)
 		}
 		if Dec64Vals.val1.err != nil || Dec64Vals.val2.err != nil || Dec64Vals.expected.err != nil {
-			fmt.Printf("\nError parsing in test: %s: \n val 1:%s: \n Val2: %s\n val 3: %s \n", testVals[i].name, Dec64Vals.val1.err, Dec64Vals.val1.err, Dec64Vals.expected.err)
+			fmt.Printf("\nError parsing in test: %s: \n val 1:%s: \n Val2: %s\n val 3: %s \n", testVals[i].testName, Dec64Vals.val1.err, Dec64Vals.val1.err, Dec64Vals.expected.err)
 
 		}
 	}
@@ -60,22 +60,22 @@ func getInput(file string) (data []testCaseStrings) {
 	dat, _ := ioutil.ReadFile("dectest/ddAdd.decTest")
 	dataString := string(dat)
 	r := regexp.MustCompile((`(?:\n)` + // start with newline (?: non capturing group)
-		`(?P<name>dd[\w]*)` + // first capturing group: testfunc made of anything that isn't a whitespace
+		`(?P<testName>dd[\w]*)` + // first capturing group: testfunc made of anything that isn't a whitespace
 		`(?:\s*)` + // match any whitespace (?: non capturing group)
-		`(?P<op>[\S]*)` + // testfunc made of anything that isn't a whitespace
+		`(?P<testFunc>[\S]*)` + // testfunc made of anything that isn't a whitespace
 		`(?:\s*\'?)` + // after can be any number of spaces and quotations if they exist (?: non capturing group)
 		`(?P<val1>\+?-?[^\t\f\v\' ]*)` + // first test val is anything that isnt a whitespace or a quoteation mark
 		`(?:'?\s*'?)` + // match any quotation marks and any space (?: non capturing group)
 		`(?P<val2>\+?-?[^->]?[^\t\f\v\' ]*)` + //testvals2 same as 1 but specifically dont match with '->'
 		`(?:'?\s*->\s*'?)` + // matches the indicator to answer and surrounding whitespaces (?: non capturing group)
 		`(?P<expectedResult>\+?-?[^\r\n\t\f\v\' ]*)`)) // matches the answer that's anything that is plus minus but not quotations
-	// capturing gorups are testName, TestFunct, TestVals_1,  TestVals_2, and answer)
+	// capturing gorups are testtestName, TestFunct, TestVals_1,  TestVals_2, and answer)
 
 	ans := r.FindAllStringSubmatch(dataString, -1)
 	for _, a := range ans {
 		data = append(data, testCaseStrings{
-			name:           a[1],
-			op:             a[2],
+			testName:       a[1],
+			testFunc:       a[2],
 			val1:           a[3],
 			val2:           a[4],
 			expectedResult: a[5]})
@@ -95,7 +95,7 @@ func convertToDec64(testvals testCaseStrings) (dec64Vals decValContainer) {
 
 // doTest completes the tests and returns a boolean and string on if the test passes
 func doTest(testVals decValContainer, testValStrings testCaseStrings) (testFailed bool, testString string) {
-	switch testValStrings.op {
+	switch testValStrings.testFunc {
 	case "add":
 		testString = fmt.Sprintf("%v + %v != %v \n(expected %v)", testValStrings.val1, testValStrings.val2, testVals.val1.d.Add(testVals.val2.d), testVals.expected.d)
 		if testVals.expected.d.Cmp(testVals.val1.d.Add(testVals.val2.d)) != 0 {
