1fa10da9924841c969fe132ebfb8a83ee7e7fd74
Joshua <joshuacarpeggiani@gmail.com>
2019-03-25 11:06:27 +1100

    Add support for 3 input tests for tests like FMA

--- decimalSuite_test.go
+++ decimalSuite_test.go
@@ -8,14 +8,15 @@ import (
 )
 
 type decValContainer struct {
-	val1, val2, expected Decimal64
-	parseError           error
+	val1, val2, val3, expected Decimal64
+	parseError                 error
 }
 type testCaseStrings struct {
 	testName       string
 	testFunc       string
 	val1           string
 	val2           string
+	val3           string
 	expectedResult string
 }
 
@@ -39,6 +40,8 @@ var tests = []string{"dectest/ddAdd.decTest"}
 // 	"dectest/ddMinus.decTest",
 // }
 
+// TODO(joshcarp): This test cannot fail. Proper assertions will be added once the whole suite passes
+// TestFromSuite is the master tester for the dectest suite.
 func TestFromSuite(t *testing.T) {
 	if RUNSUITES {
 		for _, file := range tests {
@@ -61,17 +64,6 @@ func TestFromSuite(t *testing.T) {
 	}
 }
 
-// TODO: any tests that are failing a particular test in the test suite will be turned into a unit test.
-// func TestNew(t *testing.T) {
-// require := require.New(t)
-// propper rounding
-// require.Equal(MustParseDecimal64("4444444444444445"), MustParseDecimal64("4444444444444444").Add(MustParseDecimal64("0.5001")))
-// require.Equal(MustParseDecimal64("0.23"), MustParseDecimal64("1.3").Add(MustParseDecimal64("-1.07")))
-// fmt.Println("sjoidgf", MustParseDecimal64("4444444444444444").Add(MustParseDecimal64("1.5001")))
-// require.Equal(MustParseDecimal64("12345678901234.29"), MustParseDecimal64("12345678901234").Add(MustParseDecimal64("0.2951")))
-//
-// }
-
 // TODO get regexto match with three inputs for functions like FMA.
 // getInput gets the test file and extracts test using regex, then returns a map object and a list of test names.
 func getInput(file string) (data []testCaseStrings) {
@@ -84,7 +76,9 @@ func getInput(file string) (data []testCaseStrings) {
 		`(?:\s*\'?)` + // after can be any number of spaces and quotations if they exist (?: non capturing group)
 		`(?P<val1>\+?-?[^\t\f\v\' ]*)` + // first test val is anything that isnt a whitespace or a quoteation mark
 		`(?:'?\s*'?)` + // match any quotation marks and any space (?: non capturing group)
-		`(?P<val2>\+?-?[^->]?[^\t\f\v\' ]*)` + //testvals2 same as 1 but specifically dont match with '->'
+		`(?P<val2>\+?-?[^\t\f\v\' ]*)` + // second test val is anything that isnt a whitespace or a quoteation mark
+		`(?:'?\s*'?)` +
+		`(?P<val3>\+?-?[^->]?[^\t\f\v\' ]*)` + //testvals3 same as 1 but specifically dont match with '->'
 		`(?:'?\s*->\s*'?)` + // matches the indicator to answer and surrounding whitespaces (?: non capturing group)
 		`(?P<expectedResult>\+?-?[^\r\n\t\f\v\' ]*)`) // matches the answer that's anything that is plus minus but not quotations
 	// capturing gorups are testName, testFunc, val1,  val2, and expectedResult)
@@ -95,25 +89,28 @@ func getInput(file string) (data []testCaseStrings) {
 			testFunc:       a[2],
 			val1:           a[3],
 			val2:           a[4],
-			expectedResult: a[5],
+			val3:           a[5],
+			expectedResult: a[6],
 		})
 	}
 	return
-
 }
 
 // convertToDec64 converts the map object strings to decimal64s.
 func convertToDec64(testvals testCaseStrings) (dec64vals decValContainer) {
-	var err1, err2, expectedErr error
+	var err1, err2, err3, expectedErr error
 	dec64vals.val1, err1 = ParseDecimal64(testvals.val1)
 	dec64vals.val2, err2 = ParseDecimal64(testvals.val2)
+	dec64vals.val3, err3 = ParseDecimal64(testvals.val3)
+
 	dec64vals.expected, expectedErr = ParseDecimal64(testvals.expectedResult)
 
 	if err1 != nil || err2 != nil || expectedErr != nil {
-		dec64vals.parseError = fmt.Errorf("\nerror parsing in test: %s: \n val 1:%s: \n val 2: %s\n expected: %s ",
+		dec64vals.parseError = fmt.Errorf("\nerror parsing in test: %s: \n val 1:%s: \n val 2: %s  \n val 3: %s\n expected: %s ",
 			testvals.testName,
 			err1,
 			err2,
+			err3,
 			expectedErr)
 	}
 	return
@@ -121,22 +118,19 @@ func convertToDec64(testvals testCaseStrings) (dec64vals decValContainer) {
 
 // runTest completes the tests and returns a boolean and string on if the test passes.
 func runTest(testVals decValContainer, testValStrings testCaseStrings) error {
-	calcRestul := execOp(testVals.val1, testVals.val2, testValStrings.testFunc)
-	flavor1, _, _, _ := calcRestul.parts()
-	flavor2, _, _, _ := testVals.expected.parts()
-	if flavor1 == flSNaN || flavor2 == flSNaN {
-		if testVals.expected.Cmp(calcRestul) == -2 {
-			return nil
+	calcRestul := execOp(testVals.val1, testVals.val2, testVals.val3, testValStrings.testFunc)
+	if calcRestul.IsNaN() || testVals.expected.IsNaN() {
+		if testVals.expected.String() != calcRestul.String() {
+			return fmt.Errorf(
+				"\n failed NaN TEST %s \n %v %s %v == %v \n expected result: %v ",
+				testValStrings.testName,
+				testValStrings.val1,
+				testValStrings.testFunc,
+				testValStrings.val2,
+				calcRestul,
+				testValStrings.expectedResult)
 		}
-
-		return fmt.Errorf(
-			"\nfailed NaN %s \n %v %s %v == %v \n expected result: %v ",
-			testValStrings.testName,
-			testValStrings.val1,
-			testValStrings.testFunc,
-			testValStrings.val2,
-			calcRestul,
-			testVals.expected)
+		return nil
 
 	} else if testVals.expected.Cmp(calcRestul) != 0 {
 		return fmt.Errorf(
@@ -146,14 +140,14 @@ func runTest(testVals decValContainer, testValStrings testCaseStrings) error {
 			testValStrings.testFunc,
 			testValStrings.val2,
 			calcRestul,
-			testVals.expected)
+			testValStrings.expectedResult)
 	}
 	return nil
 }
 
 // TODO: get runTest to run more functions such as FMA.
 // execOp returns the calculated answer to the operation as Decimal64.
-func execOp(val1, val2 Decimal64, op string) Decimal64 {
+func execOp(val1, val2, val3 Decimal64, op string) Decimal64 {
 	switch op {
 	case "add":
 		return val1.Add(val2)
@@ -163,7 +157,10 @@ func execOp(val1, val2 Decimal64, op string) Decimal64 {
 		return val1.Abs()
 	case "divide":
 		return val1.Quo(val2)
+	case "fma": // TODO: Add FMA function
+		//return val1.FMA(val2, val3)
 	default:
-		panic("end of operation function no tests ran")
+		fmt.Println("end of execOp, no tests ran", op)
 	}
+	return Zero64
 }
