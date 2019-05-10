582d2ad97cc29fc09dd2df629ef721544573b406
joshcarp <joshuacarpeggiani@gmail.com>
2019-05-10 16:30:07 +1000

    Reoptimise decimal test suite test

--- decimalSuite_test.go
+++ decimalSuite_test.go
@@ -1,8 +1,9 @@
 package decimal
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"regexp"
 	"testing"
 )
@@ -18,6 +19,7 @@ type testCaseStrings struct {
 	val2           string
 	val3           string
 	expectedResult string
+	rounding       string
 }
 
 const PrintFiles bool = true
@@ -41,6 +43,9 @@ var tests = []string{"",
 	// 	"dectest/ddMinMag.decTest",
 	// 	"dectest/ddMinus.decTest",
 }
+var supportedRounding = []string{"half_up",
+	"half_even"}
+var ignoredFunctions = []string{"apply"}
 
 // TODO(joshcarp): This test cannot fail. Proper assertions will be added once the whole suite passes
 // TestFromSuite is the master tester for the dectest suite.
@@ -50,23 +55,34 @@ func TestFromSuite(t *testing.T) {
 			if PrintFiles {
 				fmt.Println("starting test:", file)
 			}
-			dat, _ := ioutil.ReadFile(file)
-			dataString := string(dat)
-			testVals := getInput(dataString)
-			numTests := len(testVals)
+			f, _ := os.Open(file)
+			scanner := bufio.NewScanner(f)
+			numTests := 0
 			failedTests := 0
-			for _, testVal := range testVals {
-				dec64vals := convertToDec64(testVal)
-				calcRestul, testErr := runTest(dec64vals, testVal)
-				if PrintTests {
-					fmt.Printf("%s %s %v %v %v -> %v\n", testVal.testName, testVal.testFunc, testVal.val1, testVal.val2, testVal.val3, testVal.expectedResult)
+			var roundingSupported bool
+			var scannedContext Context64
+
+			for scanner.Scan() {
+				testVal, err := getInput(scanner.Text())
+				if testVal.rounding != "" {
+					roundingSupported = isInList(testVal.rounding, supportedRounding)
+					scannedContext = setRoundingFromString(testVal.rounding)
 				}
-				if testErr != nil && !(isRoundingErr(calcRestul, dec64vals.expected) && IgnoreRounding) {
-					fmt.Println(testErr)
-					failedTests++
-					fmt.Printf("%s %s %v %v %v -> %v\n", testVal.testName, testVal.testFunc, testVal.val1, testVal.val2, testVal.val3, testVal.expectedResult)
-					if dec64vals.parseError != nil {
-						fmt.Println(dec64vals.parseError)
+				if err == nil && roundingSupported {
+					numTests++
+					dec64vals := convertToDec64(testVal)
+					calcRestul, testErr := runTest(scannedContext, dec64vals, testVal)
+					if PrintTests {
+						fmt.Printf("%s %s %v %v %v -> %v\n", testVal.testName, testVal.testFunc, testVal.val1, testVal.val2, testVal.val3, testVal.expectedResult)
+					}
+					if testErr != nil && !(isRoundingErr(calcRestul, dec64vals.expected) && IgnoreRounding) {
+						fmt.Println(testErr)
+						fmt.Println("Rounding mode:", supportedRounding[scannedContext.roundingMode])
+						failedTests++
+						fmt.Printf("%s %s %v %v %v -> %v\n", testVal.testName, testVal.testFunc, testVal.val1, testVal.val2, testVal.val3, testVal.expectedResult)
+						if dec64vals.parseError != nil {
+							fmt.Println(dec64vals.parseError)
+						}
 					}
 				}
 			}
@@ -78,6 +94,26 @@ func TestFromSuite(t *testing.T) {
 	}
 }
 
+func isInList(s string, list []string) bool {
+	for _, item := range list {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func setRoundingFromString(s string) Context64 {
+	switch s {
+	case "half_even":
+		return Context64{roundHalfEven}
+	case "half_up":
+		return Context64{roundHalfUp}
+	default:
+		return DefaultContext
+	}
+
+}
 func isRoundingErr(res, expected Decimal64) bool {
 	resP := res.getParts()
 	expectedP := expected.getParts()
@@ -93,32 +129,45 @@ func isRoundingErr(res, expected Decimal64) bool {
 }
 
 // getInput gets the test file and extracts test using regex, then returns a map object and a list of test names.
-func getInput(file string) (data []testCaseStrings) {
-	r := regexp.MustCompile(`(?:\n)` + // start with newline (?: non capturing group)
+func getInput(line string) (testCaseStrings, error) {
+	testRegex := regexp.MustCompile(
 		`(?P<testName>dd[\w]*)` + // first capturing group: testfunc made of anything that isn't a whitespace
-		`(?:\s*)` + // match any whitespace (?: non capturing group)
-		`(?P<testFunc>[\S]*)` + // testfunc made of anything that isn't a whitespace
-		`(?:\s*\'?)` + // after can be any number of spaces and quotations if they exist (?: non capturing group)
-		`(?P<val1>\+?-?[^\t\f\v\' ]*)` + // first test val is anything that isnt a whitespace or a quoteation mark
-		`(?:'?\s*'?)` + // match any quotation marks and any space (?: non capturing group)
-		`(?P<val2>\+?-?[^\t\f\v\' ]*)` + // second test val is anything that isnt a whitespace or a quoteation mark
-		`(?:'?\s*'?)` +
-		`(?P<val3>\+?-?[^->]?[^\t\f\v\' ]*)` + //testvals3 same as 1 but specifically dont match with '->'
-		`(?:'?\s*->\s*'?)` + // matches the indicator to answer and surrounding whitespaces (?: non capturing group)
-		`(?P<expectedResult>\+?-?[^\r\n\t\f\v\' ]*)`) // matches the answer that's anything that is plus minus but not quotations
+			`(?:\s*)` + // match any whitespace (?: non capturing group)
+			`(?P<testFunc>[\S]*)` + // testfunc made of anything that isn't a whitespace
+			`(?:\s*\'?)` + // after can be any number of spaces and quotations if they exist (?: non capturing group)
+			`(?P<val1>\+?-?[^\t\f\v\' ]*)` + // first test val is anything that isnt a whitespace or a quoteation mark
+			`(?:'?\s*'?)` + // match any quotation marks and any space (?: non capturing group)
+			`(?P<val2>\+?-?[^\t\f\v\' ]*)` + // second test val is anything that isnt a whitespace or a quoteation mark
+			`(?:'?\s*'?)` +
+			`(?P<val3>\+?-?[^->]?[^\t\f\v\' ]*)` + //testvals3 same as 1 but specifically dont match with '->'
+			`(?:'?\s*->\s*'?)` + // matches the indicator to answer and surrounding whitespaces (?: non capturing group)
+			`(?P<expectedResult>\+?-?[^\r\n\t\f\v\' ]*)`) // matches the answer that's anything that is plus minus but not quotations
+
+	// Add regex to match to  rounding: rounding mode her
+
 	// capturing gorups are testName, testFunc, val1,  val2, and expectedResult)
-	ans := r.FindAllStringSubmatch(file, -1)
-	for _, a := range ans {
-		data = append(data, testCaseStrings{
-			testName:       a[1],
-			testFunc:       a[2],
-			val1:           a[3],
-			val2:           a[4],
-			val3:           a[5],
-			expectedResult: a[6],
-		})
+	ans := testRegex.FindStringSubmatch(line)
+
+	if len(ans) == 0 {
+		roundingRegex := regexp.MustCompile(`(?:rounding:[\s]*)(?P<rounding>[\S]*)`)
+		ans = roundingRegex.FindStringSubmatch(line)
+		if len(ans) == 0 {
+			return testCaseStrings{}, fmt.Errorf("No test cases")
+		}
+		return testCaseStrings{rounding: ans[1]}, fmt.Errorf("No test cases or rounding")
 	}
-	return
+	if isInList(ans[2], ignoredFunctions) {
+		return testCaseStrings{}, fmt.Errorf("No test cases")
+	}
+	data := testCaseStrings{
+		testName:       ans[1],
+		testFunc:       ans[2],
+		val1:           ans[3],
+		val2:           ans[4],
+		val3:           ans[5],
+		expectedResult: ans[6],
+	}
+	return data, nil
 }
 
 // convertToDec64 converts the map object strings to decimal64s.
@@ -127,7 +176,6 @@ func convertToDec64(testvals testCaseStrings) (dec64vals decValContainer) {
 	dec64vals.val1, err1 = ParseDecimal64(testvals.val1)
 	dec64vals.val2, err2 = ParseDecimal64(testvals.val2)
 	dec64vals.val3, err3 = ParseDecimal64(testvals.val3)
-
 	dec64vals.expected, expectedErr = ParseDecimal64(testvals.expectedResult)
 
 	if err1 != nil || err2 != nil || expectedErr != nil {
@@ -142,8 +190,8 @@ func convertToDec64(testvals testCaseStrings) (dec64vals decValContainer) {
 }
 
 // runTest completes the tests and returns a boolean and string on if the test passes.
-func runTest(testVals decValContainer, testValStrings testCaseStrings) (Decimal64, error) {
-	calcRestul := execOp(testVals.val1, testVals.val2, testVals.val3, testValStrings.testFunc)
+func runTest(context Context64, testVals decValContainer, testValStrings testCaseStrings) (Decimal64, error) {
+	calcRestul := execOp(context, testVals, testValStrings.testFunc)
 	if calcRestul.IsNaN() || testVals.expected.IsNaN() {
 		if testVals.expected.String() != calcRestul.String() {
 			return calcRestul, fmt.Errorf(
@@ -173,27 +221,27 @@ func runTest(testVals decValContainer, testValStrings testCaseStrings) (Decimal6
 
 // TODO: get runTest to run more functions such as FMA.
 // execOp returns the calculated answer to the operation as Decimal64.
-func execOp(val1, val2, val3 Decimal64, op string) Decimal64 {
+func execOp(context Context64, testVals decValContainer, op string) Decimal64 {
 	if IgnorePanics {
 		defer func() {
 			if r := recover(); r != nil {
-				fmt.Println("failed", r, val1, val2)
+				fmt.Println("failed", r, testVals.val1, testVals.val2)
 			}
 		}()
 	}
 	switch op {
 	case "add":
-		return val1.Add(val2)
+		return context.Add(testVals.val1, testVals.val2)
 	case "multiply":
-		return val1.Mul(val2)
+		return context.Mul(testVals.val1, testVals.val2)
 	case "abs":
-		return val1.Abs()
+		return testVals.val1.Abs()
 	case "divide":
-		return val1.Quo(val2)
+		return testVals.val1.Quo(testVals.val2)
 	case "fma":
-		return val1.FMA(val2, val3)
+		return context.FMA(testVals.val1, testVals.val2, testVals.val3)
 	case "compare":
-		return NewDecimal64FromInt64(int64(val1.Cmp(val2)))
+		return NewDecimal64FromInt64(int64(testVals.val1.Cmp(testVals.val2)))
 	default:
 		fmt.Println("end of execOp, no tests ran", op)
 	}
